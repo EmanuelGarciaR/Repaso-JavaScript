626e344397219876c4f38d5cff8dc4da45a5c762
EmanuelGarcia <emanuelgarciarios@gmail.com>
2025-01-13 18:26:39 -0500

    Ejercicio KOFLA 1 - Nivel Junior

---
+++ Junior/Kofla1/helados.js
@@ -0,0 +1,25 @@
+/*
+3 Chicos de 23 años perfectamente normales entran a una heladeria
+a comprar un helado pero hay un problema:
+Los precion no están al lado de cada estante con su respectivo healdo.
+Ellos quieren comprar el helado más caro que puedan con la plata que tienen
+
+Roberto tiene 1.5 USD
+Pedro tiene 1.7 USD
+Kofla tiene 3 USD
+
+Los precios de los helados son los siguientes:
+palito de helado de agua: 0.6 usd
+palito de helado de crema: 1 usd
+bombon helado marca heladix: 1.6 usd
+bombon healdo marca heladovich: 1.7 usd
+bombon helado marca helardo 1.8 usd
+potecido de helado con confites 2.9 usd
+pote de 1/4 KG -> 2.9 USD
+
+
+Crear Soluciones:
+Pedirles que ingresen el monto que tienen y mostrarles el helado más caro
+Si hay 2 o más con el mismo precio, mostrar ambos
+Kofla quiere saber cuando es el vuelto
+*/
